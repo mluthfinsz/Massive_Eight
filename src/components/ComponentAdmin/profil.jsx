b084f791b2cee1dc3da2d9e0228743cd714bb50e
mluthfinsz <mluthfisubektia27n@gmail.com>
2024-06-11 12:02:10 +0700

    First Commit

---
+++ src/components/ComponentAdmin/profil.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../ComponentAdmin/component.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const ProfilAdmin = ({ children }) => {
+  return (
+    <div className="profile-wrapper">
+      <div
+        className="col is-fixed-top has-shadow col-profile"
+        role="navigation"
+      >
+        <div className="profile-container">
+          <div className="profile-image">
+            <i className="bi bi-person-fill"></i>
+          </div>
+          <span className="username">
+            <b>Username</b>
+          </span>
+        </div>
+      </div>
+      <div className="profile-content">{children}</div>
+    </div>
+  );
+};
+
+export default ProfilAdmin;
